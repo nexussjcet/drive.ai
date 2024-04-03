3b0edad73439c7e5e485fb2e97188e5996e7932b
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-04-03 18:18:29 +0530

    added nav

---
+++ src/components/madeup/nav.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { CircleUser } from "lucide-react";
+
+type Props = {};
+
+const Nav = (props: Props) => {
+  const user = true;
+  return (
+    <>
+      {user && (
+        <div className=" m-1 flex h-14 flex-col items-center justify-center rounded-md">
+          <div className="self-end p-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="rounded-full"
+                >
+                  <CircleUser className="h-5 w-5" />
+                  <span className="sr-only">Toggle user menu</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                align="end"
+                className="absolute right-0 top-12"
+              >
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Nav;
