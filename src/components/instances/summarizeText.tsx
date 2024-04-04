788ac290e9ccff777fc13dc8f11b54991e0d7da9
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-04-04 13:59:08 +0530

    feat: ui change

--- src/components/instances/summarizeText.tsx
+++ src/components/instances/summarizeText.tsx
@@ -2,8 +2,8 @@ import { type AFC } from "@/lib/schema";
 
 export const summarizeText:AFC<"summarizeText"> = ({data}) => {
     return ( 
-        <div>
-            <div>{data.text}</div>
+        <div className="p-4 bg-gray-100">
+            <div className="line-clamp-6">{data.text}</div>
         </div>
      );
 }
